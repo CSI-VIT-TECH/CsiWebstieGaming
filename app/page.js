a4232ce522fedc027496ec635a5c39af3f206cec
SwiftByte6 <rohitsoneji6@gmail.com>
2025-10-07 23:23:26 +0530

    make the team page responsive and login page done

--- app/page.js
+++ app/page.js
@@ -3,15 +3,17 @@
 import AboutUs from "@/components/Home/AboutUs";
 import Hero from "@/components/Home/Hero";
 import HeroScale from "@/components/Home/HeroScale";
+import Navbar from "@/components/Home/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <div>
-        {/* <Hero/>
-        <AboutUs/> */}
-        <HeroScale/>
+        <Navbar />
+        <Hero/>
+        <AboutUs/>
+        {/* <HeroScale/> */}
       </div>
     </>
   );
